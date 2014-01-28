9b5c44fdc9e499ee8ab5b8c94a1135104b8540ea
Developer <dev@coalminesoftware.com>
2014-01-28 01:08:06 -0700

    Initial commit without testing.

---
+++ src/main/java/com/coalminesoftware/urimatcher/EnumUriMatcher.java
@@ -0,0 +1,69 @@
+package com.coalminesoftware.urimatcher;
+
+import android.content.UriMatcher;
+import android.net.Uri;
+
+/** Wraps an Android UriMatcher so as to allow users to associate URI matches with enumeration
+ * values, rather than arbitrary integer constants.  This reduces the amount of boilerplate code
+ * needed to do URI matching, provides type safety, allows for useful compile-time warnings (i.e.,
+ * missing cases in a switch(EnumType) statement) and eliminates the potential for errors while
+ * manually defining "code" values. */
+public class EnumUriMatcher<EnumType extends Enum<?>> {
+	private UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+	private String defaultAuthority;
+	private EnumType[] enumValues;
+
+
+	/** Creates a matcher for the given enumeration type, without a default authority.  When adding
+	 * matches, use {@link #addMatch(String,String,Enum)} or call {@link #setDefaultAuthority(String)}
+	 * before calling {@link #addMatch(String,Enum)}. */
+	public EnumUriMatcher(Class<EnumType> clazz) {
+		enumValues = clazz.getEnumConstants();
+	}
+
+	/** Creates a matcher for the given enumeration type, also specifying the authority to use when
+	 * adding a match without specifying the authority. */
+	public EnumUriMatcher(Class<EnumType> clazz, String defaultAuthority) {
+		this(clazz);
+
+		this.defaultAuthority = defaultAuthority;
+	}
+
+	/** Adds a URI match for the given authority and path, which will return the given enumeration
+	 * value when matched. */
+	public void addMatch(String authority, String uriPath, EnumType matchEnum) {
+		matcher.addURI(authority, uriPath, matchEnum.ordinal());
+	}
+
+	/**
+	 * Adds a URI match for the given path and the default authority, which will return the given enumeration
+	 * value when matched.
+	 * @throws IllegalStateException thrown if called with no default authority set.
+	 */
+	public void addMatch(String uriPath, EnumType matchEnum) {
+		if(defaultAuthority==null) {
+			throw new IllegalStateException("A default authority is needed before calling addMatch(String,EnumType).");
+		}
+
+		addMatch(defaultAuthority, uriPath, matchEnum);
+	}
+
+	public EnumType match(Uri uri) {
+		if(uri == null) {
+			return null;
+		}
+
+		int matchValue = matcher.match(uri);
+
+		return matchValue == UriMatcher.NO_MATCH?
+				null :
+				enumValues[matchValue];
+	}
+
+	/** Sets the authority used when adding a URI match using {@link #addMatch(String, Enum)}. */
+	public void setDefaultAuthority(String defaultAuthority) {
+		this.defaultAuthority = defaultAuthority;
+	}
+}
+
+
---
+++ src/test/java/com/coalminesoftware/urimatcher/EnumUriMatcherTest.java
@@ -0,0 +1,29 @@
+package com.coalminesoftware.urimatcher;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class EnumUriMatcherTest {
+	@BeforeClass
+	public static void setUpBeforeClass()
+			throws Exception {}
+
+	@Test
+	public void testAddMatchStringStringEnumType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddMatchStringEnumType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMatch() {
+		fail("Not yet implemented");
+	}
+}
+
+
